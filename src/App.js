7166dc7913c5abbced4f8480923e54eb8f57d323
MDharunPrasad <dharunprasad9494@gmail.com>
2024-08-29 21:17:44 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from "./app.module.css";
+import Footer from "./components/Footer/Footer";
+import Generator from "./components/Generator/Generator";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <Navbar />
+      <Generator />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Navbar.module.css";
+
+export default function Navbar() {
+  return (
+    <div className={styles.navbar}>
+      <p>
+        QR Generator Using React-js
+      </p>
+    </div>
+  );
+}
